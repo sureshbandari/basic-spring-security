57f85a7b6d23137d42f70965f37554cae8054ace
suresh <sureshbandari1987@gmail.com>
2023-06-12 18:09:41 -0400

    initial source code added

---
+++ src/main/java/com/suresh/basicspringsecurity/BasicSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.suresh.basicspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringSecurityApplication.class, args);
+	}
+
+}
